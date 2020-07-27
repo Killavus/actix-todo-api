f59542f9c45653ef2e15ef5d787d6f4b21983d09
Marcin Grzywaczewski <killavus@gmail.com>
2020-07-28 01:05:39 +0200

    preparation to support PostgreSQL

--- schemas/pgsql.sql
+++ schemas/pgsql.sql
@@ -1,11 +1,11 @@
 CREATE TABLE clients (
-  id INTEGER PRIMARY KEY NOT NULL,
-  display_name TEXT NOT NULL
+	id SERIAL PRIMARY KEY NOT NULL,
+	display_name TEXT NOT NULL
 );
 
 CREATE TABLE client_api_keys (
-  id INTEGER PRIMARY KEY NOT NULL,
-  valid_to INTEGER NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
+  valid_to TIMESTAMP WITH TIME ZONE NOT NULL,
   key TEXT NOT NULL,
   client_id INTEGER NOT NULL,
   FOREIGN KEY(client_id) REFERENCES clients(id)
@@ -13,17 +13,17 @@ CREATE TABLE client_api_keys (
 
 CREATE INDEX client_api_valid_to_index ON client_api_keys(client_id, valid_to);
 
+CREATE TABLE work_lists (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  client_id INTEGER NOT NULL,
+  FOREIGN KEY(client_id) REFERENCES clients(id)
+);
+
 CREATE TABLE todos (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   content TEXT NOT NULL,
   completed BOOLEAN NOT NULL,
   work_list_id INTEGER NOT NULL,
   FOREIGN KEY(work_list_id) REFERENCES work_lists(id)
-);
-
-CREATE TABLE work_lists (
-  id INTEGER PRIMARY KEY NOT NULL,
-  name TEXT NOT NULL,
-  client_id INTEGER NOT NULL,
-  FOREIGN KEY(client_id) REFERENCES clients(id)
 );
